72d8398e6ac0e27219c431f63b83694d6343f082
dineshkumar0202 <aesthetic.dinesh02@gmail.com>
2026-05-31 00:55:13 +0530

    new

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || 'https://cpu-monitor-server.onrender.com/api'
+  baseURL: 'https://cpu-monitor-server.onrender.com' || import.meta.env.VITE_API_URL
 });
 
 export default api;
+  
